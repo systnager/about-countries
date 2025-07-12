8583ab04d292cfa6f8b81e44601e82786d501806
Taras Nahornyi <systnager@gmail.com>
2025-07-12 22:24:21 +0300

    add theme icon layout

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,6 +1,12 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const isLightTheme = ref(true)
+</script>
+
 <template>
-  <nav class="p-5 bg-gray-100">
-    <div class="flex items-center justify-center gap-5">
+  <nav class="flex items-center justify-between p-5 bg-gray-100">
+    <div class="flex items-center justify-center gap-5 flex-1">
       <RouterLink class="text-xl px-5 py-2 hover:shadow-xl rounded-lg transition" to="/"
         >Main</RouterLink
       >
@@ -10,6 +16,14 @@
       <RouterLink class="text-xl px-5 py-2 hover:shadow-xl rounded-lg transition" to="/favorites"
         >Favorites</RouterLink
       >
+    </div>
+    <div>
+      <img
+        class="w-10 h-10 cursor-pointer transition hover:scale-110 active:scale-90"
+        :src="isLightTheme ? '/light-theme.png' : '/dark-theme.png'"
+        alt="change theme"
+        @click="isLightTheme = !isLightTheme"
+      />
     </div>
   </nav>
 </template>
