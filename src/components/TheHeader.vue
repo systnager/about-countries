06b43bf2449ee5844ad490ee102e73283adc573d
Taras Nahornyi <systnager@gmail.com>
2025-07-12 22:16:25 +0300

    add analytics and favorites template pages, add link in navbar

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,5 +1,15 @@
 <template>
-  <nav>
-    <h1>HEADER</h1>
+  <nav class="p-5 bg-gray-100">
+    <div class="flex items-center justify-center gap-5">
+      <RouterLink class="text-xl px-5 py-2 hover:shadow-xl rounded-lg transition" to="/"
+        >Main</RouterLink
+      >
+      <RouterLink class="text-xl px-5 py-2 hover:shadow-xl rounded-lg transition" to="/analytics"
+        >Analytics</RouterLink
+      >
+      <RouterLink class="text-xl px-5 py-2 hover:shadow-xl rounded-lg transition" to="/favorites"
+        >Favorites</RouterLink
+      >
+    </div>
   </nav>
 </template>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,14 @@
 import { createMemoryHistory, createRouter } from 'vue-router'
 
 import TheCountriesListPage from '@/pages/TheCountriesListPage.vue'
+import TheAnalyticsPage from '@/pages/TheAnalyticsPage.vue'
+import TheFavoritesPage from '@/pages/TheFavoritesPage.vue'
 
-const routes = [{ path: '/', component: TheCountriesListPage }]
+const routes = [
+  { path: '/', component: TheCountriesListPage },
+  { path: '/analytics', component: TheAnalyticsPage },
+  { path: '/favorites', component: TheFavoritesPage },
+]
 
 export const router = createRouter({
   history: createMemoryHistory(),
