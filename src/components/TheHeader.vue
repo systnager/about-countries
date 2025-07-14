f10554b432a6a53e6e79e8a73f9be7c872025adc
Taras Nahornyi <systnager@gmail.com>
2025-07-14 10:34:41 +0300

    add multi lang

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,10 +1,9 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { useI18n } from 'vue-i18n'
 import { useTheme } from '@/composables/useTheme'
+const { t, locale } = useI18n()
 
 const { isDark, toggleDark } = useTheme()
-
-const choicedLang = ref('ukr')
 </script>
 
 <template>
@@ -12,42 +11,42 @@ const choicedLang = ref('ukr')
     <RouterLink
       class="text-xl px-5 py-2 font-bold w-64 cursor-pointer transition hover:scale-110 active:scale-90"
       to="/"
-      >About Countries</RouterLink
+      >{{ t('message.title') }}</RouterLink
     >
     <div class="flex items-center justify-center gap-5 flex-1">
       <RouterLink
         class="text-xl px-5 py-2 cursor-pointer transition hover:scale-110 active:scale-90"
         to="/"
-        >Main</RouterLink
+        >{{ t('message.main') }}</RouterLink
       >
       <RouterLink
         class="text-xl px-5 py-2 cursor-pointer transition hover:scale-110 active:scale-90"
         to="/analytics"
-        >Analytics</RouterLink
+        >{{ t('message.analytics') }}</RouterLink
       >
       <RouterLink
         class="text-xl px-5 py-2 cursor-pointer transition hover:scale-110 active:scale-90"
         to="/favorites"
-        >Favorites</RouterLink
+        >{{ t('message.favorites') }}</RouterLink
       >
     </div>
     <div class="flex items-center justify-center gap-5 w-64">
       <img
         class="w-10 h-10 cursor-pointer transition hover:scale-110 active:scale-90"
-        :src="choicedLang === 'ukr' ? '/select-ukraine.png' : '/unselect-ukraine.png'"
-        alt="Choice Ukrainian"
-        @click="choicedLang = 'ukr'"
+        :src="locale === 'ukr' ? '/select-ukraine.png' : '/unselect-ukraine.png'"
+        :alt="t('message.choice ukr')"
+        @click="locale = 'ukr'"
       />
       <img
         class="w-10 h-10 cursor-pointer transition hover:scale-110 active:scale-90"
-        :src="choicedLang === 'eng' ? '/select-en.png' : '/unselect-en.png'"
-        alt="Choice English"
-        @click="choicedLang = 'eng'"
+        :src="locale === 'eng' ? '/select-en.png' : '/unselect-en.png'"
+        :alt="t('message.choice eng')"
+        @click="locale = 'eng'"
       />
       <img
         class="w-10 h-10 cursor-pointer transition hover:scale-110 active:scale-90"
         :src="isDark ? '/dark-theme.png' : '/light-theme.png'"
-        alt="change theme"
+        :alt="t('message.change theme')"
         @click="toggleDark()"
       />
     </div>
--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,31 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import { router } from '@/router/index'
+import { createI18n } from 'vue-i18n'
+import ukr from '@/locales/ukr.json'
+import eng from '@/locales/eng.json'
 
 import App from './App.vue'
 
+const i18n = createI18n({
+  legacy: false,
+  locale: 'ukr',
+  fallbackLocate: 'eng',
+  messages: {
+    ukr,
+    eng,
+  },
+})
 const app = createApp(App)
+
+console.log({
+  messages: {
+    urk: ukr,
+    eng: eng,
+  },
+})
 
 app.use(createPinia())
 app.use(router)
+app.use(i18n)
 app.mount('#app')
