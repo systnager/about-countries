1d132d84c64025ac897a5539779789e62f20f60e
Taras Nahornyi <systnager@gmail.com>
2025-07-15 15:40:22 +0300

    add sort and filters

--- src/pages/TheCountriesListPage.vue
+++ src/pages/TheCountriesListPage.vue
@@ -1,18 +1,108 @@
 <script setup lang="ts">
-import { onMounted } from 'vue'
+import { onMounted, ref } from 'vue'
+import { watchDebounced } from '@vueuse/core'
+import type { Country } from '@/types/Countries'
+import { useI18n } from 'vue-i18n'
 import CountryCardList from '@/components/CountryCardList.vue'
-import { useCountries, fetchCountries } from '@/services/CountriesService'
+import {
+  useCountries,
+  fetchCountries,
+  sortCountriesByPopulation,
+  sortCountriesByArea,
+  sortCountriesByPopulationDensity,
+  filterCountriesByName,
+  filterCountriesByRegion,
+  filterCountriesByLanguage,
+} from '@/services/CountriesService'
 
+const { t } = useI18n()
 const { countries } = useCountries()
 
-onMounted(() => {
-  fetchCountries()
-  console.log(countries)
+const filteredCountries = ref<Country[]>([])
+
+const filterByNameInput = ref<string>('')
+const filterByRegionInput = ref<string>('')
+const filterByLanguageInput = ref<string>('')
+
+function onSortCountries(value: string) {
+  if (value === 'byPopulation') {
+    sortCountriesByPopulation()
+  } else if (value === 'byArea') {
+    sortCountriesByArea()
+  } else if (value === 'byPopulationDensity') {
+    sortCountriesByPopulationDensity()
+  }
+  filterCountries()
+}
+
+function filterCountries() {
+  filteredCountries.value = countries.value
+  if (filterByNameInput.value) {
+    filteredCountries.value = filterCountriesByName(
+      filteredCountries.value,
+      filterByNameInput.value,
+    )
+  }
+  if (filterByRegionInput.value) {
+    filteredCountries.value = filterCountriesByRegion(
+      filteredCountries.value,
+      filterByRegionInput.value,
+    )
+  }
+  if (filterByLanguageInput.value) {
+    filteredCountries.value = filterCountriesByLanguage(
+      filteredCountries.value,
+      filterByLanguageInput.value,
+    )
+  }
+}
+
+onMounted(async () => {
+  await fetchCountries()
+  filteredCountries.value = countries.value
 })
+
+watchDebounced(
+  [filterByNameInput, filterByRegionInput, filterByLanguageInput],
+  () => {
+    filterCountries()
+  },
+  { debounce: 500 },
+)
 </script>
 
 <template>
   <div>
-    <CountryCardList :countries="countries" />
+    <div class="flex justify-between m-5">
+      <div class="flex gap-3">
+        <select
+          class="w-52 h-9 bg-while dark:bg-gray-600 px-5 px-2 rounded-md text-black dark:text-white shadow-md"
+          @change="onSortCountries(($event.target as HTMLSelectElement).value || '')"
+        >
+          <option value="" disabled selected>{{ t('message.sort by') }}</option>
+          <option value="byPopulation">{{ t('message.by population') }}</option>
+          <option value="byArea">{{ t('message.by area') }}</option>
+          <option value="byPopulationDensity">{{ t('message.by population density') }}</option>
+        </select>
+        <input
+          class="w-52 h-9 bg-while dark:bg-gray-600 px-5 px-2 rounded-md text-black dark:text-white shadow-md"
+          :placeholder="t('message.by name')"
+          v-model="filterByNameInput"
+        />
+      </div>
+      <div class="flex gap-3">
+        <input
+          class="w-52 h-9 bg-while dark:bg-gray-600 px-5 px-2 rounded-md text-black dark:text-white shadow-md"
+          :placeholder="t('message.by continent')"
+          v-model="filterByRegionInput"
+        />
+        <input
+          class="w-52 h-9 bg-while dark:bg-gray-600 px-5 px-2 rounded-md text-black dark:text-white shadow-md"
+          :placeholder="t('message.by language')"
+          v-model="filterByLanguageInput"
+        />
+      </div>
+    </div>
+    <CountryCardList :countries="filteredCountries" />
   </div>
 </template>
--- src/services/CountriesService.ts
+++ src/services/CountriesService.ts
@@ -1,7 +1,9 @@
 import { ref } from 'vue'
 import axios from 'axios'
 
-const countries = ref([])
+import { type Country } from '@/types/Countries'
+
+const countries = ref<Country[]>([])
 
 export function useCountries() {
   return {
@@ -18,4 +20,39 @@ export async function fetchCountries() {
   } catch (error) {
     console.error(error)
   }
+}
+
+export function sortCountriesByPopulation() {
+  countries.value.sort((a: Country, b: Country) => b.population - a.population)
+}
+
+export function sortCountriesByArea() {
+  countries.value.sort((a: Country, b: Country) => b.area - a.area)
+}
+
+export function sortCountriesByPopulationDensity() {
+  countries.value.sort((a: Country, b: Country) => b.population / b.area - a.population / a.area)
+}
+
+export function filterCountriesByRegion(countries: Country[], region: string) {
+  return countries.filter((country: Country) =>
+    country.region.toLowerCase().includes(region.toLowerCase()),
+  )
+}
+
+export function filterCountriesByLanguage(countries: Country[], language: string) {
+  return countries.filter((country: Country) => {
+    for (const key in country.languages) {
+      if (country.languages[key].toLowerCase().includes(language.toLowerCase())) {
+        return true
+      }
+    }
+    return false
+  })
+}
+
+export function filterCountriesByName(countries: Country[], name: string) {
+  return countries.filter((country: Country) =>
+    country.name.official.toLowerCase().includes(name.toLowerCase()),
+  )
 }
