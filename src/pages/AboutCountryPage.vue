5cfcc83fe8603f1c3077bb9f3307c3285839981a
Taras Nahornyi <systnager@gmail.com>
2025-07-21 13:44:23 +0300

    add AboutCountryPage template

---
+++ src/pages/AboutCountryPage.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+const countryCode = route.params.code
+</script>
+
+<template>{{ countryCode }}</template>
--- src/router/index.ts
+++ src/router/index.ts
@@ -3,11 +3,13 @@ import { createWebHistory, createRouter } from 'vue-router'
 import TheCountriesListPage from '@/pages/TheCountriesListPage.vue'
 import TheAnalyticsPage from '@/pages/TheAnalyticsPage.vue'
 import TheFavoritesPage from '@/pages/TheFavoritesPage.vue'
+import AboutCountryPage from '@/pages/AboutCountryPage.vue'
 
 const routes = [
   { path: '/', component: TheCountriesListPage },
   { path: '/analytics', component: TheAnalyticsPage },
   { path: '/favorites', component: TheFavoritesPage },
+  { path: '/country/:code', component: AboutCountryPage },
 ]
 
 export const router = createRouter({
