6c55d54258a88eeb22f604cf25bdf70c133a8b13
Taras Nahornyi <systnager@gmail.com>
2025-07-23 11:15:08 +0300

    insert tr into tbody

--- src/components/StatisticTable.vue
+++ src/components/StatisticTable.vue
@@ -10,28 +10,30 @@ defineProps<{
 <template>
   <div>
     <table class="w-full">
-      <tr class="bg-gray-300 dark:bg-gray-700 text-left">
-        <th
-          v-for="header in tableData.headers"
-          :key="header"
-          class="first:whitespace-nowrap first:text-left first:pl-3 last:pr-3 last:whitespace-nowrap last:text-right"
+      <tbody>
+        <tr class="bg-gray-300 dark:bg-gray-700 text-left">
+          <th
+            v-for="header in tableData.headers"
+            :key="header"
+            class="first:whitespace-nowrap first:text-left first:pl-3 last:pr-3 last:whitespace-nowrap last:text-right"
+          >
+            {{ t(header) }}
+          </th>
+        </tr>
+        <tr
+          v-for="{ id, data } in tableData.data"
+          :key="id"
+          class="odd:bg-gray-100 even:bg-gray-200 dark:odd:bg-gray-800 dark:even:bg-gray-900"
         >
-          {{ t(header) }}
-        </th>
-      </tr>
-      <tr
-        v-for="{ id, data } in tableData.data"
-        :key="id"
-        class="odd:bg-gray-100 even:bg-gray-200 dark:odd:bg-gray-800 dark:even:bg-gray-900"
-      >
-        <td
-          v-for="item in data"
-          :key="item"
-          class="first:whitespace-nowrap first:text-left first:pl-3 last:pr-3 last:whitespace-nowrap last:text-right"
-        >
-          {{ item }}
-        </td>
-      </tr>
+          <td
+            v-for="item in data"
+            :key="item"
+            class="first:whitespace-nowrap first:text-left first:pl-3 last:pr-3 last:whitespace-nowrap last:text-right"
+          >
+            {{ item }}
+          </td>
+        </tr>
+      </tbody>
     </table>
   </div>
 </template>
