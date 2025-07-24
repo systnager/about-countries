dc57b0d2eff6f5cf3b6674e8c1f68c933328bdd0
Taras Nahornyi <systnager@gmail.com>
2025-07-24 05:01:28 +0300

    update config for allow connect from another devices in local network

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,15 @@ import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), vueDevTools(), tailwindcss()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
+  },
+  server: {
+    host: true, // this enables listening on all network interfaces (0.0.0.0)
+    port: 5173, // optional, change to your preferred port
   },
 })
